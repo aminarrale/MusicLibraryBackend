23795e1c65de198dd03f1a20f2d971c849213663
Karim <aarrale12@gmail.com>
2022-08-16 13:34:32 -0500

    added postman collection

--- musiclibrary/views.py
+++ musiclibrary/views.py
@@ -1,3 +1,4 @@
+
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import SongSerializer
@@ -17,3 +18,19 @@ def songs_list(request):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def song_list(request, pk):
+    song = get_object_or_404(Song, pk=pk)
+    if request.method == 'GET':
+        serializer  = SongSerializer(song)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = SongSerializer(song, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        song.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
