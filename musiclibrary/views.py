1d168313875a7cb4efe48eb1536c1c7b1bf0df4e
Karim <aarrale12@gmail.com>
2022-08-16 11:31:17 -0500

    POST request added

--- musiclibrary/views.py
+++ musiclibrary/views.py
@@ -6,9 +6,14 @@ from musiclibrary import serializers
 from django.shortcuts import get_object_or_404
 from rest_framework import status
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def songs_list(request):
     if request.method == 'GET':
         songs = Song.objects.all()
         serializer = SongSerializer(songs, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = SongSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
