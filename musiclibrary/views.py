407ac4b5d67fa4e812cc3fa38f3c0a1d18b8864b
Karim <aarrale12@gmail.com>
2022-08-15 20:24:01 -0500

    GET request in views file

--- musiclibrary/views.py
+++ musiclibrary/views.py
@@ -1,7 +1,14 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import SongSerializer
+from .models import Song
+from musiclibrary import serializers
+from django.shortcuts import get_object_or_404
+from rest_framework import status
 
 @api_view(['GET'])
 def songs_list(request):
-
-    return Response('ok')
+    if request.method == 'GET':
+        songs = Song.objects.all()
+        serializer = SongSerializer(songs, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
